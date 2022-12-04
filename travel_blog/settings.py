e68d24d8d8549b8aabee1ff1c53e97423d6ef915
Roman Baksheev <rombaks.dev@gmail.com>
2022-12-04 22:05:06 +0500

    Add main app

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "travel_blog.main.apps.MainConfig",
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,8 @@ DATABASES = {
     },
 }
 
+# User model
+AUTH_USER_MODEL = "main.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
