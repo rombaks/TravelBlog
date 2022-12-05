2534c2722777833c6a360a76d4b0813f90ff5d3a
Roman Baksheev <rombaks.dev@gmail.com>
2022-12-05 17:19:34 +0500

    Remove unused imports

--- travel_blog/main/models/post.py
+++ travel_blog/main/models/post.py
@@ -2,28 +2,26 @@ from django.db import models
 from .user import User
 
 
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
+STATUS = ((0, "Draft"), (1, "Publish"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, 
-        on_delete= models.CASCADE,
-        related_name='blog_posts',
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
         blank=True,
         null=True,
     )
-    updated_at = models.DateTimeField(auto_now= True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.title
