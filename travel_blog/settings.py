5545a6b823759b13fea3f6bf3a9267d13409f083
Roman Baksheev <rombaks.dev@gmail.com>
2022-12-09 12:01:09 +0400

    Remove comments from settings.py

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -60,8 +60,6 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
